7fd17331dbcf1546ed41a500137d640548d57d22
roberto-mardeni <rmardeni@gmail.com>
2022-12-02 16:36:37 -0500

    Added fixed for time zones not available in registry

--- Application/aspnet-core-dotnet-core/Pages/Index.cshtml.cs
+++ Application/aspnet-core-dotnet-core/Pages/Index.cshtml.cs
@@ -14,14 +14,39 @@ namespace aspnet_core_dotnet_core.Pages
 
         public void OnGet()
         {
-            TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById(easternZoneId);
-            TimeZoneInfo cetZone = TimeZoneInfo.FindSystemTimeZoneById(cetZoneId);
-
             var currentTime = DateTime.UtcNow;
 
             ViewData["UTCDateTime"] = currentTime.ToString();
-            ViewData["ESTDateTime"] = TimeZoneInfo.ConvertTime(currentTime, easternZone).ToString();
-            ViewData["CETDateTime"] = TimeZoneInfo.ConvertTime(currentTime, cetZone).ToString();
+            ViewData["ESTDateTime"] = null;
+            ViewData["CETDateTime"] = null;
+
+            try
+            {
+                TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById(easternZoneId);
+                ViewData["ESTDateTime"] = TimeZoneInfo.ConvertTime(currentTime, easternZone).ToString();
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                ViewData["ESTDateTime"] = "The registry does not define the Eastern Standard Time zone.";
+            }
+            catch (InvalidTimeZoneException)
+            {
+                ViewData["ESTDateTime"] = "Registry data on the Eastern Standard Time zone has been corrupted.";
+            }
+
+            try
+            {
+                TimeZoneInfo cetZone = TimeZoneInfo.FindSystemTimeZoneById(cetZoneId);
+                ViewData["CETDateTime"] = TimeZoneInfo.ConvertTime(currentTime, cetZone).ToString();
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                ViewData["CETDateTime"] = "The registry does not define the Central European Standard Time zone.";
+            }
+            catch (InvalidTimeZoneException)
+            {
+                ViewData["CETDateTime"] = "Registry data on the Central European Standard Time zone has been corrupted.";
+            }
         }
         public string DoTest()
         {
