c736651ab63c40fe8b8a65e7f4507a3f4ac775fb
roberto-mardeni <rmardeni@gmail.com>
2022-12-02 16:17:29 -0500

    Added UTC, EST & CET Date Time placeholders and UI

--- Application/aspnet-core-dotnet-core/Pages/Index.cshtml.cs
+++ Application/aspnet-core-dotnet-core/Pages/Index.cshtml.cs
@@ -9,9 +9,19 @@ namespace aspnet_core_dotnet_core.Pages
 {
     public class IndexModel : PageModel
     {
+        string easternZoneId = "Eastern Standard Time";
+        string cetZoneId = "Central European Standard Time";
+
         public void OnGet()
         {
+            TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById(easternZoneId);
+            TimeZoneInfo cetZone = TimeZoneInfo.FindSystemTimeZoneById(cetZoneId);
 
+            var currentTime = DateTime.UtcNow;
+
+            ViewData["UTCDateTime"] = currentTime.ToString();
+            ViewData["ESTDateTime"] = TimeZoneInfo.ConvertTime(currentTime, easternZone).ToString();
+            ViewData["CETDateTime"] = TimeZoneInfo.ConvertTime(currentTime, cetZone).ToString();
         }
         public string DoTest()
         {
